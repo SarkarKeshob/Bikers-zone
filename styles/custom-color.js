2f66241c5480c264ece9f616189b0058a70561e8
Shop Queen Dot Com Dot Bd <keshob.sarkar.shuvo@gmail.com>
2023-11-20 04:45:10 +0600

    uploading till main section

---
+++ styles/custom-color.js
@@ -0,0 +1,10 @@
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            bikers_primary_color:'#E76F51;'
+          }
+        }
+      }
+    }
